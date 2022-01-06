a2b1c7b636d94187c58b5bc51461c5c420835cb4
jennyy <jennyy89@users.noreply.github.com>
2022-01-06 13:01:31 +0100

    fetching data from hpchar

---
+++ src/Characters.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Characters({ char }) {
+  console.log(char);
+
+  return (
+    <section>
+      {char.map((charac, index) => (
+        <div key={index}>
+          <h1>{charac.name}</h1>
+          <img src={charac.img} alt="" />
+        </div>
+      ))}
+    </section>
+  );
+}
